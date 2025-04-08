3c69b756d84bd93789b61138c95b4f872550ca4c
ElenPavliychenkova <e.pavliychenkova@gmail.com>
2025-04-08 13:44:08 +0300

    init

---
+++ src/main/java/ru/test/cards/api/cardsapi/CardsApiApplication.java
@@ -0,0 +1,13 @@
+package ru.test.cards.api.cardsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CardsApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CardsApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/test/cards/api/cardsapi/CardsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.test.cards.api.cardsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
